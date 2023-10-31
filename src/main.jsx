b4f8899233f83f50ce050d11dc6f33cb1a64d4da
Kamrul Hassan <kamrulthedev@gmail.com>
2023-10-31 15:46:51 +0600

    react-commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <div>Hello World !!!</div>
+}])
 
 
 
-const router = createBrowserRouter ([{
-  path: "/",
-  element: <div>Hello !!</div>
-},
-{
-  path: "/react",
-  element: <div>I am Frist React Router Application</div>
 
-},
-{
-  path: "/about",
-   element : <div>I am in the about page</div>
-},
-{
-  path: "/div",
-  element: <div>I am a web Developer for my paletfrom for the proggraming Hero</div>
-}
-])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
