ff1c2590f7ff8ce3e49674e26a7e774b6374bb0d
Kamrul Hassan <kamrulthedev@gmail.com>
2023-10-31 12:53:55 +0600

    react-router-commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+
+
+const router = createBrowserRouter ([{
+  path: "/",
+  element: <div>Hello !!</div>
+},
+{
+  path: "/react",
+  element: <div>I am Frist React Router Application</div>
+
+},
+{
+  path: "/about",
+   element : <div>I am in the about page</div>
+},
+{
+  path: "/div",
+  element: <div>I am a web Developer for my paletfrom for the proggraming Hero</div>
+}
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
